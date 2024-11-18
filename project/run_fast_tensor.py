9a5ce8bf477dfd9a7206c765b287ed8b148ca7bb
jacksoncamp42 <jacksoncamp42@gmail.com>
2024-11-18 16:02:40 -0500

    Added files for training analysis

--- project/run_fast_tensor.py
+++ project/run_fast_tensor.py
@@ -30,7 +30,10 @@ class Network(minitorch.Module):
 
     def forward(self, x):
         # TODO: Implement for Task 3.5.
-        raise NotImplementedError("Need to implement for Task 3.5")
+        # Similar to previous implementation but using fast operations
+        h = self.layer1.forward(x).relu()
+        h = self.layer2.forward(h).relu()
+        return self.layer3.forward(h).sigmoid()
 
 
 class Linear(minitorch.Module):
@@ -44,7 +47,15 @@ class Linear(minitorch.Module):
 
     def forward(self, x):
         # TODO: Implement for Task 3.5.
-        raise NotImplementedError("Need to implement for Task 3.5")
+        # Use efficient matrix multiplication
+        batch, in_size = x.shape
+        w = self.weights.value.view(in_size, self.out_size)
+
+        # Efficient matmul implementation
+        out = x @ w
+
+        # Add bias - broadcasting will handle batch dimension
+        return out + self.bias.value
 
 
 class FastTrain:
